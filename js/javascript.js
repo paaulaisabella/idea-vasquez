8c04209c4c102a040605bfeddb7a05adf3aa7f39
Paula <paulaisabellavasquez@gmail.com>
2021-10-25 22:40:26 -0300

    hice un algoritmo corto en JS

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,15 @@
 let nombre = prompt("Por favor, ingresa tu nombre");
-let edad = prompt("Ahora, ingresa tu edad")
-console.log("Hola " + nombre)
+let edad = prompt("Ahora, ingresa tu edad");
 
+const mensaje = alert("Mi nombre es " + nombre + " y tengo " + edad + " años de edad");
+
+const mensajeDos = alert("Ahora haremos matemáticas");
+
+let num1 = prompt("Ingresa un número");
+let num2 = prompt("Ingresa otro número");
+
+let num3 = Number(num1) + Number(num2); 
+
+const resultado1 = alert("La suma de tus números es " + num3);
+
+const  final = alert("Gracias por participar, " + nombre);
