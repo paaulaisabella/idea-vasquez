ef0e4c6ca2b6441d8db56edfdbfd66ec64966e06
Paula <paulaisabellavasquez@gmail.com>
2021-10-27 23:04:20 -0300

    Hice el desafío de los condicionales con un ejercicio de rentas de carros

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,26 @@
-let nombre = prompt("Por favor, ingresa tu nombre");
-let edad = prompt("Ahora, ingresa tu edad");
+/* Una compañía dedicada al alquiler de automoviles cobra un monto fijo de $300000 para los primeros 300 km de recorrido. 
+Para más de 300 km y hasta 1000 km, cobra un monto adicional de $15.000 por cada kilómetro en exceso sobre 300. 
+Para más de 1000 km cobra un monto adicional de $ 10.000 por cada kilómetro en exceso sobre 1000. 
+Los precios ya incluyen el 20% del impuesto general a las ventas, IVA. Diseñe un algoritmo que determine el monto a pagar por el alquiler de un vehículo y el monto incluído del impuesto. */
 
-const mensaje = alert("Mi nombre es " + nombre + " y tengo " + edad + " años de edad");
+let km = prompt("Por favor, ingresa la cantidad de kilómetros recorridos");
+let montoFijo = 300000;
+let montoAdicional1 = 15000;
+let montoAdicional2 = 10000;
+let iva = 0.2;
 
-const mensajeDos = alert("Ahora haremos matemáticas");
+const recorrido = Number(km)
 
-let num1 = prompt("Ingresa un número");
-let num2 = prompt("Ingresa otro número");
+let valor1 = (recorrido - 300) * montoAdicional1; 
+let valor2 = (recorrido - 1000) * montoAdicional2; 
 
-let num3 = Number(num1) + Number(num2); 
+let subTotal1 = valor1 + montoFijo;
+let subTotal2 = valor2 + montoFijo;
 
-const resultado1 = alert("La suma de tus números es " + num3);
-
-const  final = alert("Gracias por participar, " + nombre);
+if (km <= 300) {
+    alert("El costo total es $" + montoFijo);
+} else if (km > 300 && km <= 1000) {
+    alert("El costo total es igual a $" + subTotal1 + ". Y el total con el IVA es igual a $" + ((subTotal1 * iva) + subTotal1));
+} else {
+    alert("El costo total es igual a $" + subTotal2 + ". Y el total con el IVA es igual a $" + ((subTotal2 * iva) + subTotal2));
+}
