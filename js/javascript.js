dda0404363841b38ebcb38d1644ce990975a9d8d
Paula <paulaisabellavasquez@gmail.com>
2021-11-09 00:13:23 -0300

    hice el desafío de los objetos

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,27 @@
-let tareas = Number(prompt("Ingresa la cantidad de tareas a realizar"));
+let cantTareas = Number(prompt("Ingrese la cantidad de tareas a realizar."));
 
+class Tareas {
+    constructor(nombre, duracion, categoria){
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.categoria = categoria
+    }
 
-function pedirInfo() {
-    let nombreTarea = prompt("Ingresa el nombre de la tarea");
-    let tiempoTarea = Number(prompt("Ingresa el tiempo de duración"));
-
-    alert(`El nombre de la tarea es ${nombreTarea} y su duración es ${tiempoTarea}`);
+    alertaUsuario(){
+        if(this.duracion > 200) { 
+            alert("Ingrese un tiempo válido.");
+        } else if (this.duracion < 0) {
+            alert("Usted ingresó una cantidad inválida, intente de nuevo.");
+        } else {
+            alert(`La tarea se llama ${this.nombre} y dura ${this.duracion}min. Su categoría es: ${this.categoria}`);
+        } 
+    }
 }
 
-
-for (i = 0; i < tareas; i++) {
-    pedirInfo();
+for (let i = 0; i < cantTareas; i++) {
+    let nombreTarea = prompt("Ingrese el nombre de la tarea.");
+    let tiempoTarea = Number(prompt("Ingrese el tiempo de duración (en minutos)."));
+    let categTarea = prompt("Ahora, indique la categoría de la tarea (escuela, trabajo, hogar...).");
+    let tarea = new Tareas (nombreTarea, tiempoTarea, categTarea);
+    tarea.alertaUsuario();
 }
-
