5c14abac1f76065eb48293060cf7003b2a555ca3
Paula <paulaisabellavasquez@gmail.com>
2021-11-10 00:34:23 -0300

    Arreglé un poco el código de js y le agregué un array para hacer una lista

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,11 @@
+
+// VARIABLE PARA MOSTRAR LA LISTA DE TAREAS:
+let lista = [];
+
+// VARIABLE DE CANTIDAD DE TAREAS:
 let cantTareas;
+
+// INICIA BUCLE PARA VER CANT. DE TAREAS
 do {
     cantTareas = Number(prompt("Ingrese la cantidad de tareas a realizar."));
 if (cantTareas <= 0) {
@@ -8,6 +15,8 @@ if (cantTareas <= 0) {
 }
 } while (cantTareas <= 0 || cantTareas > 99);
 
+
+// DECLARAMOS UNA CLASE CON SU MÉTODO:
 class Tareas {
     constructor(nombre, duracion, categoria){
         this.nombre = nombre;
@@ -20,18 +29,22 @@ class Tareas {
             alert("Ingrese un tiempo válido.");
         } else if (this.duracion < 0) {
             alert("Usted ingresó un tiempo inválido, intente de nuevo.");
-        } else if (this.duracion == NaN) {
-                alert("Por favor ingrese un número.");
         } else {
             alert(`La tarea se llama ${this.nombre} y dura ${this.duracion}min. Su categoría es: ${this.categoria}`);
         } 
     }
 }
 
+// INICIAMOS UN FOR PARA PEDIR DATOS DE LAS TAREAS A REALIZAR: 
 for (let i = 0; i < cantTareas; i++) {
     let nombreTarea = prompt("Ingrese el nombre de la tarea.");
     let tiempoTarea = Number(prompt("Ingrese el tiempo de duración (en minutos)."));
     let categTarea = prompt("Ahora, indique la categoría de la tarea (escuela, trabajo, hogar...).");
-    let tarea = new Tareas (nombreTarea, tiempoTarea, categTarea);
+
+    const tarea = new Tareas (nombreTarea, tiempoTarea, categTarea);
+
     tarea.alertaUsuario();
+    lista.push(tarea);
 }
+
+console.log(lista);
