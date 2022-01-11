a6a7ebabfa5f6947051ebf66c6f50c72fcf4b7ed
Paula <paulaisabellavasquez@gmail.com>
2022-01-10 21:25:02 -0300

    Hice unos cuantos cambios en el proyecto. Al final terminó siendo una lista de tareas simple, agregas la tarea y se muestra a un lado, se van agregando a un array y se pueden ir borrando las tareas que quieran (logré que funcionara el botón), puede ser de manera individual o todas juntas, todo esto se guarda en el local storage. También trabaja con una API que indica los feriados del 2022 en Argentina, al presionar el botón muestra los resultados de la API.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,7 @@ $( document ).ready(function() {
 });
 
 // VARIABLE PARA MOSTRAR LA LISTA DE TAREAS:
-let lista = [];
-
-// FUNCIONES ------------------------------------------------
-
+let lista = []; //COMENZAMOS EL ARRAY PARA PODER UTILIZARLO MÁS ADELANTE
 
 // LS
 function actualizarLS(lista) {
@@ -23,184 +20,120 @@ lista = JSON.parse(tareasLS);
 mostrarTareas(lista);
 }
 
+//CREAMOS UNA CLASE CON SU CONSTRUCTOR PARA LOS OBJETOS
+class Tareas {
+    constructor(nombre, descripcion){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.id = Date.now();
+    }
+
+    //GENERAMOS UN MÉTODO PARA CUANDO EL CAMPO DEL NOMBRE ESTÉ VACÍO, ASÍ LE INDICAMOS AL USUARIO QUE LO LLENE
+    mostrarError(){
+        let error = document.createElement("p");
+        error.textContent = "Debes escribir el nombre de la tarea.";
+        error.className = "texto-resaltado";
+        error.style.marginTop = "1rem";
+        let lista = document.querySelector(".contenedor__tareas");
+        lista.appendChild(error);
+
+        //ESTO HACE QUE LA ALERTA DURE 3 SEGUNDOS Y NO SE QUEDE ESCRITA EN EL CUADRO
+        setTimeout(() => {
+            error.remove();
+        }, 3000
+        );
+    }
+}
 //ANIMACIÓN PARA QUE EL TÍTULO APAREZCA Y DESAPAREZCA AL CARGAR LA PÁGINA 
 $("h1").fadeOut(2000, function(){
     $("h1").fadeIn(2000);
 }); 
 
-// TAREAS
-
-function mostrarTareas(lista) { 
+function mostrarTareas() {
+    //LIMPIAMOS EL HTML CUANDO SE AGREGAN MÁS TAREAS
     const mostrarTareas = document.querySelector(".lista__tareas");
     mostrarTareas.innerHTML = "";
     
     lista.forEach(tarea => { 
         let nombre = tarea.nombre;
-        let duracion = tarea.duracion;
         let descripcion = tarea.descripcion;
+        let id = tarea.id;
 
-        /* const li = document.createElement("li");
-        li.className = "list-item"
-        li.innerHTML = `<div class= "inner-task"><span class= "texto-resaltado">Nombre: </span> ${nombre}
-        <span class= "texto-resaltado">Duración: </span> ${duracion} min. 
-        <span class= "texto-resaltado">Descripción: </span> ${descripcion} </div>
-        <button type= "button" name= "button" id= "button" class= "borrar-item">X</button>
-        `;
-        mostrarTareas.appendChild(li); */
-
-        /* CON JQUERY */
-        $(".lista__tareas").append(`<li class = "list-item"><div class= "inner-task"><span class= "texto-resaltado">Nombre: </span> ${nombre}
-        <span class= "texto-resaltado">Duración: </span> ${duracion} min. 
-        <span class= "texto-resaltado">Descripción: </span> ${descripcion} </div>
-        <button type= "button" name= "button" id= "button" class= "borrar-item">X</button></li>`)
+        $(".lista__tareas").append(`<li class = "list-item"><div class = "inner-task">
+        <p class = "texto" ><span class = "texto-resaltado">Nombre:</span> ${nombre}<br>
+        <span class = "texto-resaltado"> Descripción: </span> ${descripcion}</div></p>
+        <button type= "button" name= "button" id= "${id}" class= "borrar-item">X</button></li>`)
 
         });
 }
 
-$("#comenzar").click(function (e) { //FUNCIÓN PARA COMENZAR EL TEMPORIZADOR
-    e.preventDefault();
-    $(".contenedor__tareas").fadeOut(); //QUITAMOS EL FORMULARIO PARA QUE NO SE AGREGUEN MÁS TAREAS MIENTRAS CORRE EL TIMER
-    $(".primer-contenedor").css("justify-content", "center");
-    for (let i = 0; i < lista.length; i++){
-        let duracion = lista[i].duracion;
-        let tiempo = duracion * 60;
-        
-        let recargarIntervalo = setInterval(actualizarConteo, 1000);
-        
-        function actualizarConteo(){
-            const minutos = Math.floor(tiempo / 60);
-            let segundos = tiempo % 60;
-        
-            segundos = segundos < 10 ? '0' + segundos : segundos;
-        
-            let seccion = document.querySelector(".seccion");
-            seccion.innerHTML = " "; //AGREGAMOS ESTO PARA QUE SE VAYAN BORRANDO LOS NÚMEROS Y NO SE ACUMULEN AL LADO DEL OTRO
-            let parrafo = document.createElement("p");
-            parrafo.textContent = ` ${minutos}:${segundos} `
-            seccion.appendChild(parrafo);
-            
-            tiempo--;
-            if (tiempo < 0) { // ESTO PARA QUE NO SE VAYA A NÚMEROS NEGATIVOS
-                clearInterval(recargarIntervalo);
-            }
-            if (tiempo == 0){
-                $(".contenedor__tareas").fadeIn("slow");
-            }
-        }
-        
-        if(i >= lista.length) continue
-        actualizarConteo(recargarIntervalo);
-        let contenedor = document.querySelector(".contenedor__cuadro-tareas");
-        contenedor.innerHTML += " ";
-        $(".contenedor__cuadro-tareas").append(`<p class = "llevando-a-cabo">${lista[i].nombre} <br>
-        "${lista[i].descripcion}"</p>`)
-    }
-});
-
-// DECLARAMOS UNA CLASE CON SU MÉTODO PARA CADA TAREA
-class Tareas {
-    constructor(nombre, duracion, descripcion){
-        this.nombre = nombre;
-        this.duracion = duracion;
-        this.descripcion = descripcion;
-    }
-
-    // AGREGAMOS UNA ALERTA POR SI INGRESAN UN NÚMERO INVÁLIDO
-    alertaTiempo(){
-        if(this.duracion > 200) { 
-            /* const mayor200 = document.querySelector(".contenedor__tareas");
-            let p = document.createElement("p");
-            p.style.color = "red"
-            p.style.fontSize = "15px"
-            p.textContent = "El tiempo debe ser menor a 200.";
-            mayor200.appendChild(p); */
-
-            /* CON JQUERY */
-            $(".contenedor__tareas").append(`<p class= "alerta-usuario">El tiempo debe ser menor a 200min</p>`);
-        } else if (this.duracion <= 0) {
-
-            $(".contenedor__tareas").append(`<p class= "alerta-usuario">El tiempo debe ser mayor a 0min</p>`);
-        }
-    }
-
-}
-
-/* EVENTOS */
-
-    $("#formulario").submit((e) => { // HACEMOS CLICK PARA ENVIAR EL FORMULARIO, ESTAS TAREAS SE VAN AGREGANDO AL ARRAY
+$("#formulario").submit((e) => { //HACEMOS CLICK PARA ENVIAR EL FORMULARIO, ESTAS TAREAS SE VAN AGREGANDO AL ARRAY
         e.preventDefault();
         
         let nombreTarea = document.getElementById("nombre-tarea").value;
-        let tiempoTarea = Number(document.getElementById("tiempo-tarea").value);
         let descripTarea = document.getElementById("descripcion-tarea").value;
 
-        const tarea = new Tareas (nombreTarea, tiempoTarea, descripTarea);
+        const tarea = new Tareas (nombreTarea, descripTarea);
 
-        //AGREGAMOS UNA CONDICIÓN PARA EVITAR QUE SE AGREGUEN TAREAS VACÍAS, SIN TIEMPO O CON TIEMPO MAYOR A 200
-        if (nombreTarea !== "" && tiempoTarea !== 0 && tiempoTarea <= 200 && tiempoTarea > 0) {
+        //AGREGAMOS UNA CONDICIÓN PARA EVITAR QUE SE AGREGUEN TAREAS VACÍAS
+        if (nombreTarea) {
             const vacio = document.querySelector(".contenedor__tareas");
             vacio.innerHTML += "";
             lista.push(tarea);
-        } 
+        } else {
+            tarea.mostrarError(); //CORREMOS EL MÉTODO QUE AVISA SI EL INPUT DEL NOMBRE ESTÁ VACÍO
+        }
         
         //EJECUTAMOS LAS FUNCIONES CORRESPONDIENTES
-        tarea.alertaTiempo();
         actualizarLS(lista);
         document.getElementById("formulario").reset(); // ESTO PARA QUE EL FORMULARIO SE LIMPIE CADA VEZ QUE AGREGAMOS UNA TAREA
         mostrarTareas(lista);
     })
 
-    /* BOTONES--------------------------------------- */
-    
-    // AGREGAMOS UN BOTÓN A CADA TAREA PARA ELIMIARLA INDIVIDUALMENTE (ESTO TODAVÍA NO LA BORRA DEL LS)
-    const quitarItem = document.querySelectorAll('.borrar-item');
-    for (let i = 0; i < quitarItem.length; i++){
-        quitarItemEvento = quitarItem[i].addEventListener("click", (e) => {
-            const boton = e.target
-            boton.closest('.list-item').remove();
-        });
+
+    //EVENTO PARA BORRAR LAS TAREAS DE MANERA INDIVIDUAL
+$(".contenedor__cuadro-porHacer").click((e) => {
+    if (e.target.tagName == 'BUTTON'){ //SI EL ELEMENTO SELECCIONADO DENTRO DEL CONTENEDOR ES UN BOTÓN, SE PROCEDE A BORRAR LA TAREA
+        const deleteId = parseInt(e.target.getAttribute('id'));
+        lista = lista.filter(tarea => tarea.id !== deleteId); //DEVUELVE EL ARRAY SIN LA TAREA SELECCIONADA
+        
+        //CORREMOS LAS FUNCIONES CORRESPONDIENTES
+        actualizarLS(lista);
+        mostrarTareas(lista);
     }
-
-    // AGREGAMOS UN BOTÓN PARA BORRAR TODO EL LOCALSTORAGE
-
-    /* const borrar = document.querySelector(".boton-borrar").addEventListener("click", borrarTareas);
-    function borrarTareas() {
-        localStorage.clear();
-        lista = [];
-        document.querySelector(".lista__tareas").innerHTML = lista;
-    } */
-
-    /* CON JQUERY */
-    $(".boton-borrar").click(() => {
-        localStorage.clear();
-        lista = [];
-        document.querySelector(".lista__tareas").innerHTML = lista;
     })
+
+    //AGREGAMOS UN BOTÓN PARA BORRAR TODA LA LISTA 
+$(".boton-borrar").click(() => {
+    localStorage.clear(); //ACÁ NOS ASEGURAMOS DE BORRARLO DEL LS 
+    lista = [];
+    document.querySelector(".lista__tareas").innerHTML = lista;
+})
 
 // AJAX
 
-    $("#fecha").click(function (e) { //ESTE BOTÓN ES PARA PODER ACCEDER A LOS DATOS DE LA API DE FERIADOS
-        e.preventDefault();
-        $("#fecha").hide();
-        for(let i = 0; i < 16; i++){ //UTILIZAMOS UN FOR PARA QUE RECORRA TODOS LOS DATOS DEL ARRAY
-            let mostrarDatos = (fecha) => {
-                $(".tercer-contenedor").append(`<p class = "parrafo-feriados">El ${fecha[i].date} es ${fecha[i].localName}</p>`)
-                $(".parrafo-feriados").css({"margin": "0% 5% 3%",
-                                    "background-color": "rgba(178, 178, 179, 0.502)",
-                                    "border-radius": "3px",
-                                    "padding": "3px"});
-            }
-    
-            let url = "https://date.nager.at/api/v3/publicholidays/2022/AR"
-            const getData = () => {
-                $.ajax({
-                    method: "GET",
-                    url: url,
-                    success: (response) => {
-                        mostrarDatos(response);
-                    }
-                });
-            }
-            getData();
+$("#fecha").click(function (e) { //ESTE BOTÓN ES PARA PODER ACCEDER A LOS DATOS DE LA API DE FERIADOS
+    e.preventDefault();
+    $("#fecha").hide();
+    for(let i = 0; i < 16; i++){ //UTILIZAMOS UN FOR PARA QUE RECORRA TODOS LOS DATOS DEL ARRAY
+        let mostrarDatos = (fecha) => {
+            $(".segundo-contenedor").append(`<p class = "parrafo-feriados">El ${fecha[i].date} es ${fecha[i].localName}</p>`)
+            $(".parrafo-feriados").css({"margin": "0% 5% 3%",
+                                "background-color": "#189ad29d",
+                                "border-radius": "3px",
+                                "padding": "3px"});
         }
-    });
+
+        let url = "https://date.nager.at/api/v3/publicholidays/2022/AR"
+        const getData = () => {
+            $.ajax({
+                method: "GET",
+                url: url,
+                success: (response) => {
+                    mostrarDatos(response);
+                }
+            });
+        }
+        getData();
+    }
+});
